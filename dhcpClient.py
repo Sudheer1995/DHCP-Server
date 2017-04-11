bcdce7e34275fd5b19cc736ab7859ae63dd4de68
teja <pbteja1998@gmail.com>
2017-04-11 23:58:46 +0530

    First Version

--- dhcpClient.py
+++ dhcpClient.py
@@ -3,26 +3,43 @@ import socket
 
 class dhcpClient:
 	''' client requests methods for a connection to dhcp Server '''
-	def __init__(self):
+	def __init__(self, mac):
 		''' initilize the port and open a socket for other end of
 			communication requesting a connection dhcp Server '''
 		self.clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.clientPort = 6000
 	 	self.hostname = socket.gethostname()
 		self.clientSocket.connect((self.hostname, self.clientPort))
+		self.mac = mac
 	
-	def clientDiscover():
+	def clientDiscover(self):
 		''' send the request for dhcp server for a connection '''
+		self.clientSocket.send(self.mac)
+		self.clientOffer()
 
-	def clientOffer():
+	def clientOffer(self):
 		''' take the IP address sent by the dhcp Server '''
+		assignedIp = self.clientSocket.recv(32)
+		self.clientRequest(assignedIp)
 
-	def clientRequest():
+	def clientRequest(self, ip):
 		''' send back the IP address sent by dhcp Server
 			for verification of proper communication channel '''
-	def clientAck():
+		self.clientSocket.send(ip)
+		self.clientAck(ip)
+
+	def clientAck(self, ip):
 		''' receive the acknowledgment sent by dhcp Server '''
+		ack = self.clientSocket.recv(4)
+		if ack:
+			self.ip = ip
+		else:
+			self.clientDiscover()
+
 
 if __name__ == "__main__":
 	
-	client = dhcpClient()
+	mac = str(raw_input())
+	client = dhcpClient(mac)
+	client.clientDiscover()
+	print client.ip
--- dhcpServer.py
+++ dhcpServer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 import socket
+import operator
+from thread import *
 
 class dhcpServer:
 	''' functionalities of dhcp Server '''
@@ -8,24 +10,106 @@ class dhcpServer:
 		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.port = 6000
 		self.hostname = socket.gethostname()
-		self.socket.bind(self.hostname, self.port)
-		self.socket.listen(5)
+		self.socket.bind((self.hostname, self.port))
+		self.socket.listen(1000)
+		self.labs = {}
+		self.macs = {}
+		self.Ips = {}
+		self.setData()
+		
 
-	def dhcpDiscover(self):
+	def setData(self):
+		
+		with open('subnets.conf','r') as subnet_file:
+		
+			line_number = 1
+
+			for line in subnet_file:
+				if line_number == 1:
+					IpDetails = line.split('/')
+					self.startIp = IpDetails[0]
+					self.numberOfIps = 2**(32 - int(IpDetails[1])) - 2				
+				elif line_number == 2:
+					self.numberOfLabs = int(line)
+				elif line_number >= 3 and line_number <= 3 + self.numberOfLabs - 1:
+					labDetails = line.split(':')
+					self.labs[labDetails[0]] = int(labDetails[1])
+				else:
+					macDetails = line.split()
+					self.macs[macDetails[0]] = macDetails[1]
+				line_number += 1
+
+		self.allocateIpsToLabs()
+
+	def allocateIpsToLabs(self):
+		labs = sorted(self.labs.items(), key=operator.itemgetter(0))
+		currentIp = self.startIp
+
+		for lab in labs:
+			self.Ips[lab[0]] = []
+			for i in range(0, lab[1]):
+				self.Ips[lab[0]].append([currentIp,0])
+				currentIpParts = currentIp.split('.')
+				currentIpParts = [int(i) for i in currentIpParts]
+				for j in range(3, -1, -1):
+					currentIpParts[j] += 1
+					if currentIpParts[j] <= 255:
+						break
+					else:
+						currentIpParts[j] = 0
+				currentIp = '.'.join(str(part) for part in currentIpParts)
+		
+
+	def dhcpDiscover(self, conn):
 		''' receive the request from client for connection '''
+		mac_address = conn.recv(17)
+		lab = self.macs[mac_address]
+		if len(self.Ips[lab]) != 0:
+			i = 0
+			while self.Ips[lab][i][1] != 0:
+				i += 1
 
-	def dhcpOffer(self):
-		''' process the request from client and assign 
-			a IP address from the pool of addresses '''
+			ipToAssign = self.Ips[lab][i][0]
+			print self.Ips[lab][i]
+		self.dhcpOffer(ipToAssign, lab, conn)
 
-	def dhcpRequest(self):
+
+	def dhcpOffer(self, ip, lab, conn):
+		''' send the ipAddress assigned to the client '''
+		conn.send(ip)
+		self.dhcpRequest(ip, lab, conn)
+
+	def dhcpRequest(self, ip, lab, conn):
 		''' receive the IP addresses sent by dhcpOffer() from 
 			client to verify proper addresses is received '''
+		assignedIp = conn.recv(32)
 
-	def dhcpAck(self):
+		if assignedIp == ip:
+			self.dhcpAck(1, lab, ip, conn)
+
+		else:
+			self.dhcpAck(0, lab, ip, conn)
+
+	def dhcpAck(self, ack, lab, ip, conn):
 		''' send an acknowledgement to client if an dhcpRequest() 
 			verification is passed otherwise send an not an acknowledgement '''
+		if ack:
+			conn.send('ACK')
+			for i in range(0, len(self.Ips[lab])):
+				if self.Ips[lab][i][0] == ip:
+					break
+			print self.Ips[lab][i]
+			self.Ips[lab][i][1] = 1
+
+		else:
+			conn.send('NACK')
+			self.dhcpDiscover(conn)
+
 
 if __name__ == "__main__":
 	
 	server = dhcpServer()
+
+	while True:
+		conn, addr = server.socket.accept()
+		start_new_thread(server.dhcpDiscover,(conn,))	
